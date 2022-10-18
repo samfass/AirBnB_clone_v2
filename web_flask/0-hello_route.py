b03d79f5d141150b68b2bce5c2e62dc9b16e6007
Fasil Solomon <98734803+samfass@users.noreply.github.com>
2022-10-18 22:35:55 +0300

    send

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -22,8 +22,11 @@ if __name__ == "__main__":
 """
 starts a Flask web application
 """
+
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
